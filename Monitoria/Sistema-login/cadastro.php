66b4cc7179ea37a43a4f881565c07fc0f5727374
Suhailah de Moraes <68478385+SuhMoraes@users.noreply.github.com>
2020-11-30 14:23:30 -0300

    Monitoria - Sistema Login

---
+++ Monitoria/Sistema-login/cadastro.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <title>Cadastra-se</title>
+        <link rel="stylesheet" href="./css/global.css">
+        <link rel="stylesheet" href="./css/cadastro.css">       
+       
+    </head>
+    <body>
+        <main>
+            <div class="card">
+                <h1>Cadastra-se</h1>
+                <form action="">
+                    <input type="text" placeholder="Digite o seu nome">
+                    <input type="email" placeholder="Digite o seu e-mail">
+                    <input type="password" placeholder="Digite a sua senha">
+                    <input type="password" placeholder="Confirme a sua senha">
+                    <button>Cadastra-se</button>
+                </form>
+                <a href="./index.php">Já possui cadastro? Realize o login</a>
+
+
+            </div>
+        </main>
+    </body>
+</html>
---
+++ Monitoria/Sistema-login/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <title>Entre ou cadastra-se</title>
+        <link rel="stylesheet" href="./css/global.css">
+        <link rel="stylesheet" href="./css/login.css">
+    </head>
+    <body>
+        <main>
+            <section class="painel-login">
+                
+                <div class="card">
+                <h1>Entre ou cadastra-se</h1>
+
+                <form action="" method="">
+                    <input type="email" placeholder="E-mail">
+                    <input type="password" placeholder="Senha">
+                    <button>Entrar</button>
+                </form>
+                <a href="./cadastro.php">Ainda não possui cadastro? Clique aqui</a>                 
+                </div>
+            </section>
+
+            <section class="painel-imagem">
+                <img src="/img/login.svg" alt="Login">
+
+            </section>
+
+        </main>
+    </body>
+</html>
